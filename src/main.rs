003a7ff3d021dbf5937bee23893029aceb6837b2
Anas Alkhatib <anas.alkhatib@gmail.com>
2025-07-23 13:07:28 -0400

    Check that file pointed to by file_path argument exists

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,13 @@ use std::process;
 
 fn main() {
     let arg = std::env::args().nth(1);
-    let Some(_path) = arg else {
+    let Some(path) = arg else {
         eprintln!("Usage: in-gen input_file.csv");
         process::exit(1);
     };
+    // check that file exists
+    if !std::path::Path::new(&path).exists() {
+        eprintln!("file {path} does not exist");
+        process::exit(1);
+    }
 }
--- tests/cli.rs
+++ tests/cli.rs
@@ -11,3 +11,16 @@ fn args_filepath_missing() -> Result<(), Box<dyn std::error::Error>> {
         .stderr(predicate::str::contains("Usage: in-gen input_file.csv"));
     Ok(())
 }
+
+#[test]
+fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("in-gen")?;
+
+    let path = "file/does/not/exist";
+    cmd.arg(path);
+    let expected_error = format!("file {path} does not exist");
+    cmd.assert()
+        .failure()
+        .stderr(predicate::str::contains(expected_error));
+    Ok(())
+}
